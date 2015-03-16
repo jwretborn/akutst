b920ba7f3d9a5dc58e20f0c4ab0e837b7301e035
Jens Wretborn <jens@wbn.se>
2015-03-16 17:51:13 +0100

    Ned Models and some template changes
    Added jquery

--- application.py
+++ application.py
@@ -16,36 +16,80 @@ from models import *
 
 @app.route("/")
 def index():
-	d = time.strftime("%Y-%m-%d")
-	procedure_types = db.session.query(ProcedureType).all()
-	return render_template('form.html', date_today=d, procedures=procedure_types)
+	return redirect(url_for('procedure'))
 
-@app.route('/procedure', methods=['GET', 'POST'])
-def procedure():
+@app.route("/ultraljud")
+def ultrasound():
+	return render_template('diagnostics.html')
+
+@app.route("/patienter")
+def patients():
+	return render_template("patients.html")
+
+@app.route('/procedurer', methods=['GET', 'POST'])
+@app.route('/procedurer/<id>', methods=['GET', 'POST'])
+def procedure(id=False):
 	errors = []
+	procedure_type = False
 	if request.method == 'POST':
+		try :
+			p = Procedure(
+				user_id=request.form['user_id'],
+				type=request.form['procedure'],
+				method=request.form['method'],
+				anatomy=request.form['anatomy'],
+				tuition=request.form['tuition'],
+				created=request.form['date'],
+				successful=request.form['successful'],
+				comments=request.form['comments']
+			)
+			db.session.add(p)
+			db.session.commit()
 
-		p = Procedure(
-			user_id=request.form['user_id'],
-			type=request.form['procedure'],
-			created=request.form['date'],
-			successful=request.form['successful'],
-			attempts=request.form['attempts']
-		)
-		db.session.add(p)
-		db.session.commit()
+			flash(u'Proceduren sparad', 'info')
+		except:
+			flash(u'Something went wrong', 'warning')
 
-		flash(u'Proceduren sparad', 'info')
+	d = time.strftime("%Y-%m-%d")
+	p_types = db.session.query(ProcedureType).all()
+	procedure_type = p_types[0]
 
-		return redirect(url_for('index'))
-	else:
-		return redirect(url_for('index'))
+	methods = []
+	anatomy = []
+
+	if procedure_type.method_group is not None :
+		methods = db.session.query(GroupItem).filter(GroupItem.group_id == procedure_type.method_group)
+
+	if procedure_type.anatomy_group is not None :
+		anatomy = db.session.query(GroupItem).filter(GroupItem.group_id == p_types[0].anatomy_group)
+	
+	return render_template('form.html', date_today=d, p_type=procedure_type, procedures=p_types, methods=methods, anatomys=anatomy)
 
 @app.route('/diagnostic', methods=['GET', 'POST'])
 def diagnostic():
 	return redirect(url_for('index'))
 
-@app.route('/group/<id>/items', methods=['GET'])
+@app.route('/api/procedure', methods=['GET'])
+@app.route('/api/procedure/<id>', methods=['GET'])
+def api_procedure(id=False):
+	if id :
+		procedure = db.session.query(Procedure).filter(Procedure.id == id).first()
+		return jsonify(procedure.serialize)
+	else :
+		procedures = db.session.query(Procedure).all()
+		return jsonify(items=[i.serialize for i in procedures])
+
+@app.route('/api/proceduretype', methods=['GET'])
+@app.route('/api/proceduretype/<id>', methods=['GET'])
+def api_procedure_type(id=False):
+	if id :
+		procedure_type = db.session.query(ProcedureType).filter(ProcedureType.id == id).first()
+		return jsonify(procedure_type.serialize)
+	else :
+		procedure_types = db.session.query(ProcedureType).all()
+		return jsonify(items=[i.serialize for i in procedure_types])
+
+@app.route('/api/group/<id>/items', methods=['GET'])
 def group_items(id):
 	query = db.session.query(Group).filter(Group.id == id)
 	group = query.first()
--- models.py
+++ models.py
@@ -1,62 +1,97 @@
 from application import db
 from sqlalchemy.dialects.postgresql import JSON
 
+"""Procedure class"""
 class Procedure(db.Model):
 	__tablename__ = 'procedures'
 
 	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-	user_id = db.Column(db.String(10))
+	user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
 	procedure_type = db.Column(db.Integer, db.ForeignKey('procedure_types.id'), nullable=False)
-	method_id = db.Column(db.Integer, db.ForeignKey('group_items.id'))
-	anatomy_id = db.Column(db.Integer, db.ForeignKey('group_items.id'))
+	method_id = db.Column(db.Integer, db.ForeignKey('group_items.id'), nullable=True)
+	anatomy_id = db.Column(db.Integer, db.ForeignKey('group_items.id'), nullable=True)
+	tuition = db.Column(db.Boolean, default=False)
 	created = db.Column(db.Date, default=db.func.now())
-	successful = db.Column(db.Boolean)
-	comments = db.Column(db.String(400))
+	successful = db.Column(db.Boolean, nullable=True)
+	comments = db.Column(db.String(400), nullable=True)
 
+	user = db.relationship("User", foreign_keys=[user_id])
 	method = db.relationship("GroupItem", foreign_keys=[method_id])
 	anatomy = db.relationship("GroupItem", foreign_keys=[anatomy_id])
 
-	def __init__(self, user_id, type, created, successful, attempts):
+	@property
+	def serialize(self):
+		"""Return object data in easily serializeable format"""
+		return {
+			'id'				: self.id,
+			'procedure_type'	: self.procedure_type,
+			'method_id'  		: self.method_id,
+			'anatomy_id'		: self.anatomy_id,
+			'tuition'			: self.tuition,
+			'created'			: str(self.created),
+			'successful'		: self.successful,
+			'comments'			: self.comments
+		}
+
+	def __init__(self, user_id, type, method, anatomy, tuition, created, successful, comments):
 		self.user_id=user_id
 		self.procedure_type=type
+		self.method_id=method
+		self.anatomy_id=anatomy
+		self.tuition=tuition
 		self.created=created
 		self.successful=successful
-		self.attempts=attempts
+		self.comments=comments
 
 	def __repr__(self):
 		return '<id {}>'.format(self.id)
 
+"""Procedure Type class"""
 class ProcedureType(db.Model):
 	__tablename__ = 'procedure_types'
 
 	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-	name = db.Column(db.String(64))
-	method_group = db.Column(db.Integer, db.ForeignKey('groups.id'))
-	anatomy_group = db.Column(db.Integer, db.ForeignKey('groups.id'))
-	show_success = db.Column(db.Boolean)
-	weight = db.Column(db.Integer)
+	name = db.Column(db.String(64), nullable=False)
+	method_group = db.Column(db.Integer, db.ForeignKey('groups.id'), nullable=True)
+	anatomy_group = db.Column(db.Integer, db.ForeignKey('groups.id'), nullable=True)
+	show_success = db.Column(db.Boolean, default=False)
+	weight = db.Column(db.Integer, nullable=False, default=0)
+
+	@property
+	def serialize(self):
+		"""Return object data in easily serializeable format"""
+		return {
+			'id'				: self.id,
+			'name'				: self.name,
+			'method_group'		: self.method_group,
+			'anatomy_group'		: self.anatomy_group,
+			'show_success'		: self.show_success,
+			'weight' 			: self.weight
+		}
 
 	def __repr__(self):
 		return "<ProcedureType(name=>'%s'" % (self.name)
 
+"""Group class"""
 class Group(db.Model):
 	__tablename__ = 'groups'
 
 	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-	name = db.Column(db.String(64))
+	name = db.Column(db.String(64), nullable=False)
 
 	items = db.relationship('GroupItem', backref="group")
 
 	def __repr__(self):
 		return '<id {}>'.format(self.id)
 
+"""Group Item class"""
 class GroupItem(db.Model):
 	__tablename__ = 'group_items'
 
 	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-	group_id = db.Column(db.Integer, db.ForeignKey('groups.id'))
-	name = db.Column(db.String(64))
-	weight = db.Column(db.Integer)
+	group_id = db.Column(db.Integer, db.ForeignKey('groups.id'), nullable=False)
+	name = db.Column(db.String(64), nullable=False)
+	weight = db.Column(db.Integer, nullable=False, default=0)
 
 	@property
 	def serialize(self):
@@ -70,6 +105,49 @@ class GroupItem(db.Model):
 
 	def __repr__(self):
 		return '<id {}>'.format(self.id)
+
+"""User class"""
+class User(db.Model):
+	__tablename__ = 'users'
+
+	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+	name = db.Column(db.String(32))
+	username = db.Column(db.String(32))
+
+	def __repr__(self):
+		return '<user {}>'.format(self.id)
+
+"""Patient class"""
+class Patient(db.Model):
+	__tablename__ = 'patients'
+
+	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+	user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+	age_id = db.Column(db.Integer, db.ForeignKey('group_items.id'), nullable=False)
+	triage_id = db.Column(db.Integer, db.ForeignKey('group_items.id'), nullable=False)
+	retts_id = db.Column(db.Integer, db.ForeignKey('retts_codes.id'), nullable=True)
+	admittance = db.Column(db.Boolean, nullable=True)
+	comments = db.Column(db.String(400))
+	created = db.Column(db.Date, default=db.func.now())
+
+	user = db.relationship("User", foreign_keys=[user_id])
+	age = db.relationship("GroupItem", foreign_keys=[age_id])
+	triage = db.relationship("GroupItem", foreign_keys=[triage_id])
+	retts = db.relationship("RettsCode", foreign_keys=[retts_id])
+
+	def __repr__(self):
+		return '<patient {}>'.format(self.id)
+
+"""Retts code class"""
+class RettsCode(db.Model):
+	__tablename__ = 'retts_codes'
+
+	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+	name = db.Column(db.String(64), nullable=False)
+	code = db.Column(db.Integer, nullable=False)
+
+	def __repr__(self):
+		return '<RETTS Code {}>'.format(self.id)
 
 #class Diagnostic(db.Model):
 #	__tablename__ = 'diagnostics'
