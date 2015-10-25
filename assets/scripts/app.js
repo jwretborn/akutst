49eb0ec52dc063ced8e791a9f71024c61ccf85ad
Jens Wretborn <jens@doubleint.com>
2015-10-25 12:13:22 +0100

    More webpack tweaking

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,7 +1,14 @@
-import React from 'react'
-import {render} from 'react-dom'
+import React, { Component } from 'react';
 
-render(
-    <h3>Hello, World from WebPack!</h3>,
-    document.getElementById("retts-react")
-)
+export default class App extends Component {
+	render() {
+		return (
+			<div>
+				<label for="retts" className="col-sm-2 control-label">Search - react</label>
+				<div className="col-sm-4">
+                	<input type="text" className="form-control" name="react-search" value="sökorsak" />
+				</div>
+			</div>
+		);
+	}
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,27 @@
 var path = require("path"),
     webpack = require("webpack"),
     ExtractTextPlugin = require("extract-text-webpack-plugin"),
-    ManifestRevisionPlugin = require("manifest-revision-webpack-plugin")
+    ManifestRevisionPlugin = require("manifest-revision-webpack-plugin"),
+    WebpackCleanupPlugin = require("webpack-cleanup-plugin")
 
+var publicHost = '';
+var buildOutputPath = './public';
 var root = "./assets"
 
 module.exports = {
     entry: {
         app_js: [
-            root + "/scripts/app.js"
+            root + "/scripts/index.js"
         ],
         main_css: [
             root + "/styles/main.scss"
         ]
     },
     output: {
-        path: "./public",
-        publicPath: "/assets/",
-        filename: "[name].[chunkhash].js",
-        chunkFilename: "[id].[chunkhash].chunk"
+        path : buildOutputPath,
+        publicPath: publicHost +"/assets/",
+        filename: "[name].[hash].js",
+        chunkFilename: "[id].[hash].chunk"
     },
     resolve: {
         extensions: ["", ".js", ".scss"]
@@ -49,6 +52,10 @@ module.exports = {
                 NODE_ENV: '"production"'
             }
         }),
-        new webpack.NoErrorsPlugin()
+        new webpack.NoErrorsPlugin(),
+        // Dev, remove unused assets
+        new WebpackCleanupPlugin({
+            exclude : []
+        })
     ]
 }
