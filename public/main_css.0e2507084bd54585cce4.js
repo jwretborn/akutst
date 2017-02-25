1d6dddc2049dd079cc7227fbb50be6fbafd7676e
Jens Wretborn <jens@wbn.se>
2017-02-25 20:19:10 +0100

    Adding registration and forgot password functionality and some db-fixes

--- public/main_css.0e2507084bd54585cce4.js
+++ public/main_css.0e2507084bd54585cce4.js
@@ -45,12 +45,12 @@
 /***/ 0:
 /***/ function(module, exports, __webpack_require__) {
 
-	module.exports = __webpack_require__(432);
+	module.exports = __webpack_require__(444);
 
 
 /***/ },
 
-/***/ 432:
+/***/ 444:
 /***/ function(module, exports) {
 
 	// removed by extract-text-webpack-plugin
